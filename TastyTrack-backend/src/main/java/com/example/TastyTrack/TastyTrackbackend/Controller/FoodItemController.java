61855d2c6a547ae40b2fc3c3e4df26b3f8bde5dd
Ehsan-Uddoula <ehsan.siam135@gmail.com>
2024-05-05 09:23:30 +0600

    Load and Upload image

--- TastyTrack-backend/src/main/java/com/example/TastyTrack/TastyTrackbackend/Controller/FoodItemController.java
+++ TastyTrack-backend/src/main/java/com/example/TastyTrack/TastyTrackbackend/Controller/FoodItemController.java
@@ -18,6 +18,7 @@ import java.nio.file.Files;
 import java.util.List;
 import java.util.Optional;
 
+@CrossOrigin("*")
 @RestController
 @RequestMapping("/api/food/")
 public class FoodItemController {
